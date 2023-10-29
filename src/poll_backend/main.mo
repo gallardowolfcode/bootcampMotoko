6cacc423e6fd4d64b2d1e69585770030c268151b
gallardowolfcode <you@example.com>
2023-10-29 17:07:04 -0600

    init

--- src/poll_backend/main.mo
+++ src/poll_backend/main.mo
@@ -1,5 +1,106 @@
+import Text "mo:base/Text";
+import RBTree "mo:base/RBTree";
+import Nat "mo:base/Nat";
+import Iter "mo:base/Iter";
+
+
 actor {
-  public query func greet(name : Text) : async Text {
-    return "Hello, " # name # "!";
+  var question: Text = "¿Qué especialidad quieres elegir en la Carrera de Ingeniería en Sistemas Computacionales?";
+  var question2: Text = "Datos";
+  var votes: RBTree.RBTree<Text, Nat> = RBTree.RBTree(Text.compare);
+  var alumnos: RBTree.RBTree<Text,Nat> = RBTree.RBTree(Text.compare);
+
+  public func getQuestion1() : async Text {
+  return "¿Qué especialidad quieres elegir en la Carrera de Ingeniería en Sistemas Computacionales?";
+};
+
+public func getQuestion2() : async Text {
+  return "Datos";
+};
+// query the list of entries and votes for each one
+// Example: 
+//      * JSON that the frontend will receive using the values above: 
+//      * [["Motoko","0"],["Python","0"],["Rust","0"],["TypeScript","0"]]
+
+  public query func getVotes() : async [(Text, Nat)] {
+    
+      Iter.toArray(votes.entries())
+    
+  };
+//   public query func getAlumnos() : async [(Text, Nat)]{
+
+//       Iter.toArray(alumnos.entiries())
+// };
+
+  // This method takes an entry to vote for, updates the data and returns the updated hashmap
+// Example input: vote("Motoko")
+// Example: 
+//      * JSON that the frontend will receive using the values above: 
+//      * [["Motoko","1"],["Python","0"],["Rust","0"],["TypeScript","0"]]
+    
+  public func vote(entry: Text) : async [(Text, Nat)] {
+
+    //Check if the entry already has votes.
+    //Note that "votes_for_entry" is of type ?Nat. This is because: 
+    // * If the entry is in the RBTree, the RBTree returns a number.
+    // * If the entry is not in the RBTree, the RBTree returns `null` for the new entry.
+    let votes_for_entry :?Nat = votes.get(entry);
+    
+    //Need to be explicit about what to do when it is null or a number so every case is taken care of
+    let current_votes_for_entry : Nat = switch votes_for_entry {
+      case null 0;
+      case (?Nat) Nat;
+    };
+
+    //once we have the number of votes, update the votes for the entry
+    votes.put(entry, current_votes_for_entry + 1);
+
+    //Return the number of votes as an array (so frontend can display it)
+    Iter.toArray(votes.entries())
+  };
+
+  public func alumno(entry: Text) : async [(Text, Nat)] {
+
+    //Check if the entry already has votes.
+    //Note that "votes_for_entry" is of type ?Nat. This is because: 
+    // * If the entry is in the RBTree, the RBTree returns a number.
+    // * If the entry is not in the RBTree, the RBTree returns `null` for the new entry.
+    let alumnos_for_entry :?Nat = alumnos.get(entry);
+    
+    //Need to be explicit about what to do when it is null or a number so every case is taken care of
+    let current_alumnos_for_entry : Nat = switch alumnos_for_entry {
+      case null 0;
+      case (?Nat) Nat;
+    };
+
+    //once we have the number of votes, update the votes for the entry
+    alumnos.put(entry, current_alumnos_for_entry + 1);
+
+    //Return the number of votes as an array (so frontend can display it)
+    Iter.toArray(alumnos.entries())
+  };
+
+  //This method resets the vote count for each option and returns the updated hashmap
+// Example JSON that the frontend will get using the values above
+// [["Motoko","0"],["Python","0"],["Rust","0"],["TypeScript","0"]]
+    
+  public func resetVotes() : async [(Text, Nat)] {
+
+    votes.put("Ciber Seguridad", 0);
+    votes.put("Internet de las Cosas", 0);
+    votes.put("Inteligencia Artificial", 0);
+    Iter.toArray(votes.entries())
+
+  };
+
+  public func resetAlumnos() : async [(Text, Nat)] {
+
+    alumnos.put("Ciber Seguridad", 0);
+    alumnos.put("Internet de las Cosas", 0);
+    alumnos.put("Inteligencia Artificial", 0);
+    Iter.toArray(alumnos.entries())
+
   };
 };
+
+
--- src/poll_frontend/src/index.js
+++ src/poll_frontend/src/index.js
@@ -1,19 +1,148 @@
 import { poll_backend } from "../../declarations/poll_backend";
+const pollForm = document.getElementById("radioForm");
+const pollForm2 = document.getElementById("radioForm2");
+const resultsDiv = document.getElementById('results');
+const resetButton = document.getElementById('reset');
 
-document.querySelector("form").addEventListener("submit", async (e) => {
+
+//1. LOCAL DATA
+const pollResults = {
+    "Ciber Seguridad": 0,
+    "Internet de las Cosas": 0,
+    "Inteligencia Artificial": 0
+};
+
+const pollResults2 = {
+  "Alumno": 0,
+  "Maestro": 0
+};
+
+//2. EVENT LISTENERS
+
+//Load the Simple Poll's question from the backend when the app loads
+document.addEventListener('DOMContentLoaded', async (e) => {
+   //note that this is at beginning of the submit callback, this is deliberate
+  //This is so the default behavior is set BEFORE the awaits are called below
   e.preventDefault();
-  const button = e.target.querySelector("button");
+  // Query the question from the backend
+  const question = await poll_backend.getQuestion();
+  document.getElementById("question").innerText = question;
 
-  const name = document.getElementById("name").value.toString();
-
-  button.setAttribute("disabled", true);
-
-  // Interact with foo actor, calling the greet method
-  const greeting = await poll_backend.greet(name);
-
-  button.removeAttribute("disabled");
-
-  document.getElementById("greeting").innerText = greeting;
-
+  //Query the vote counts for each option
+  // Example JSON that the frontend will get using the values above
+  // [["Motoko","0"],["Python","0"],["Rust","0"],["TypeScript","0"]]
+  const voteCounts = await poll_backend.getVotes();
+  updateLocalVoteCounts(voteCounts);
+  displayResults();
   return false;
-});
+}, false);
+
+document.addEventListener('DOMContentLoaded', async (e) => {
+  //note that this is at beginning of the submit callback, this is deliberate
+ //This is so the default behavior is set BEFORE the awaits are called below
+  e.preventDefault();
+  // Query the question from the backend
+  const question2 = await poll_backend.getQuestion();
+  document.getElementById("question2").innerText = question2;
+
+  //Query the vote counts for each option
+  // Example JSON that the frontend will get using the values above
+  // [["Motoko","0"],["Python","0"],["Rust","0"],["TypeScript","0"]]
+  const alumnosCounts = await poll_backend.getAlumnos();
+  updateLocalVoteCounts(alumnosCountsCounts);
+  displayResults();
+  return false;
+}, false);
+
+//Event listener that listens for when the form is submitted.
+//When the form is submitted with an option, it calls the backend canister
+//via "await poll_backend.vote(selectedOption)"
+pollForm.addEventListener('submit', async (e) => {
+  //note that this is at beginning of the submit callback, this is deliberate
+  //This is so the default behavior is set BEFORE the awaits are called below
+  e.preventDefault(); 
+
+  const formData = new FormData(pollForm);
+  const checkedValue = formData.get("option");
+
+  const updatedVoteCounts = await poll_backend.vote(checkedValue);
+  console.log("Returning from await...")
+  console.log(updatedVoteCounts);
+  updateLocalVoteCounts(updatedVoteCounts);
+  displayResults();
+  return false;
+}, false);
+
+pollForm2.addEventListener('submit', async (e) => {
+  //note that this is at beginning of the submit callback, this is deliberate
+  //This is so the default behavior is set BEFORE the awaits are called below
+  e.preventDefault(); 
+  const formData = new FormData(pollForm2);
+  const checkedValue = formData.get("option");
+
+  const updatedAlumnoCounts = await poll_backend.alumno(checkedValue);
+  console.log("Returning from await...")
+  console.log(updatedAlumnoCounts);
+  updateLocalAlumnoCounts(updatedAlumnoCounts);
+  displayResults();
+  return false;
+}, false);
+
+resetButton.addEventListener('click', async (e) => {
+
+    e.preventDefault();
+    
+    //Reset the options in the backend
+    await poll_backend.resetVotes();
+    const voteCounts = await poll_backend.getVotes();
+    updateLocalVoteCounts(voteCounts);
+
+    //re-render the results once the votes are reset in the backend
+    displayResults();
+    return false;
+}, false);
+
+//3. HELPER FUNCTIONS
+
+//Helper vanilla JS function to create the HTML to render the results of the poll
+function displayResults() {
+  let resultHTML = '<ul>';
+  for (let key in pollResults) {
+      resultHTML += '<li><strong>' + key + '</strong>: ' + pollResults[key] + '</li>';
+  }
+  resultHTML += '</ul>';
+  resultsDiv.innerHTML = resultHTML;
+};
+
+function displayResults2() {
+  let resultHTML = '<ul>';
+  for (let key in pollResults2) {
+      resultHTML += '<li><strong>' + key + '</strong>: ' + pollResults2[key] + '</li>';
+  }
+  resultHTML += '</ul>';
+  resultsDiv.innerHTML = resultHTML;
+};
+
+//This helper updates the local JS object that the browser holds
+// Example JSON that the frontend will get using the values above
+  // [["Motoko","0"],["Python","0"],["Rust","0"],["TypeScript","0"]]
+function updateLocalVoteCounts(arrayOfVoteArrays){
+
+  for (let voteArray of arrayOfVoteArrays) {
+    //Example voteArray -> ["Motoko","0"]
+    let voteOption = voteArray[0];
+    let voteCount = voteArray[1];
+    pollResults[voteOption] = voteCount;
+  }
+
+};
+
+function updateLocalAlumnoCounts(arrayOfAlumnoArrays){
+
+  for (let alumnoArray of arrayOfAlumnoArrays) {
+    let alumnoOption = alumnoArray[0];
+    let alumnoCount = alumnoArray[1];
+    pollResults2[alumnoOption] = alumnoCount;
+  }
+
+};
